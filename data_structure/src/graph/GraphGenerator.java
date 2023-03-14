6ae2cf86423a6ae788aa37bf49eb5a54625316d1
cwx0117 <cwxcwx0117@gmail.com>
2023-03-14 19:50:43 +0800

    图

---
+++ data_structure/src/graph/GraphGenerator.java
@@ -0,0 +1,27 @@
+package graph;
+
+public class GraphGenerator {
+    public static Graph createGraph(Integer[][] matrix){
+        Graph graph = new Graph();
+        for (Integer[] integers : matrix) {
+            Integer from = integers[0];
+            Integer to = integers[1];
+            Integer weight = integers[2];
+            if (!graph.nodes.containsKey(from)) {
+                graph.nodes.put(from, new Node(from));
+            }
+            if (!graph.nodes.containsKey(to)) {
+                graph.nodes.put(to, new Node(to));
+            }
+            Node fromNode = graph.nodes.get(from);
+            Node toNode = graph.nodes.get(from);
+            Edge newEdge = new Edge(weight, fromNode, toNode);
+            fromNode.nexts.add(toNode);
+            fromNode.out++;
+            toNode.in++;
+            fromNode.edges.add(newEdge);
+            graph.edges.add(newEdge);
+        }
+        return graph;
+    }
+}
